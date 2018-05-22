f643a6f8c07bf18cb3fb1fd378d258398ecb7adb
Ruturaj <rituraj@digverve.com>
2018-05-22 14:09:21 +0530

    Sch Updates

--- backup_sql.bat
+++ backup_sql.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 ECHO.
-
 ECHO Please don't close this window. Database backup is running...
 
-set TIMESTAMP=%date:~-4,4%_%date:~-7,2%_%date:~-10,2%
+SET TIMESTAMP=%date:~-4,4%_%date:~-7,2%_%date:~-10,2%
 
-if not exist "C:\R2_SQL_Backup\" mkdir C:\R2_SQL_Backup
+REM Create directory if not exist.
+IF NOT EXIST "C:\R2_SQL_Backup\" MKDIR C:\R2_SQL_Backup
 
 REM Export all databases into file C:\path\backup\databases.[year][month][day].sql
 "C:\xampp\mysql\bin\mysqldump.exe" --all-databases --result-file="C:\R2_SQL_Backup\databases_%TIMESTAMP%.sql" --user=root  --password=
@@ -20,3 +20,8 @@ MAKECAB "databases_%TIMESTAMP%.sql" "databases_%TIMESTAMP%.sql.cab"
 
 REM Delete uncompressed DB dump file.
 DEL /q /f "databases_%TIMESTAMP%.sql"
+
+REM Delete files older than 30 days.
+FORFILES
+ECHO Deleting old backup files...
+FORFILES /P "C:\R2_SQL_Backup" /S /M *.* /D -30 /C "cmd /c del @PATH"
